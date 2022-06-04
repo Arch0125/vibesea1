ccb9c9bee2d8170bf9c937dedc217078d7212733
Arch0125 <dasarchisman25@gmail.com>
2022-06-04 23:08:30 +0530

    Add Message upload

--- pages/api/components/CreatePost.tsx
+++ pages/api/components/CreatePost.tsx
@@ -5,31 +5,44 @@ import { useToast } from '@chakra-ui/react'
 import { Textarea } from '@chakra-ui/react';
 import {AiOutlineFileImage} from 'react-icons/ai'
 import { useMetamask } from './context/metamask.context';
+import PostContentABI from './smartcontracts/artifacts/contracts/PostContract.sol/PostContract.json'
+import {ethers} from 'ethers';
 
 const CreatePost =() =>{
 
+    const[textcontent,setTextcontent]=useState('');
     const toast = useToast()
     const{provider} = useMetamask();
+    const PostContractInterface = new ethers.Contract('0xDF7D3D9B7437e985fab0295C0A26f559adE07163',PostContentABI.abi,provider?.getSigner());
 
-    const showToast =() =>{
-        toast({
+
+    const addPost =() =>{
+        try{
+            PostContractInterface.addPost(textcontent,'abc');
+            toast({
             title: 'Post created.',
             description: "Refresh your page to see latest post",
             status: 'success',
             duration: 2000,
             isClosable: true,
-          })
-
-          console.log(provider)
+          })}
+          catch(err){
+            toast({
+                title: 'Wrong Network',
+                description: "Change to Ropsten, Rinkeby or Polygon Mumbai",
+                status: 'error',
+                duration: 2000,
+                isClosable: true,
+              })
+          }
     }
 
-    const[textcontent,setTextcontent]=useState('');
 
     return(
         <Box flexDirection={"column"} alignContent={"flex-end"} textAlign={"end"}>
             <Textarea onChange={e => setTextcontent(e.target.value)} placeholder= '#wagmi tell what youre buidling'/>
             <Button mr={"20px"}  marginTop={"20px"} variant={"solid"} colorScheme={"purple"}><AiOutlineFileImage/> &nbsp; Add Image</Button>
-            <Button onClick={() => showToast()} marginTop={"20px"} variant={"solid"} colorScheme={"purple"}>Create Post</Button>
+            <Button onClick={() => addPost()} marginTop={"20px"} variant={"solid"} colorScheme={"purple"}>Create Post</Button>
         </Box>
     )
 }
